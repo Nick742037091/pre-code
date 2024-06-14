e820e55699c13152c59257a27b1b2c9b2b51387f
Nick742037091 <742037091@qq.com>
2024-06-14 15:16:36 +0800

    ✨ feat(template): 增加设置模板类型按钮

--- packages/webview-page/src/pages/edit-template/index.tsx
+++ packages/webview-page/src/pages/edit-template/index.tsx
@@ -1,13 +1,53 @@
 import { CloseCircleOutlined } from '@ant-design/icons'
 import Editor, { useMonaco } from '@monaco-editor/react'
 import { useConfig } from '@/stores/config'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import classNames from 'classnames'
+
+function LanguageSelect(props: {
+  language: string
+  setLanguage: (value: string) => void
+}) {
+  const wrapperClass = 'border-solid border-2px p-4px'
+  const activeClass = classNames(wrapperClass, 'border-primary')
+  const inactiveClass = classNames(
+    wrapperClass,
+    'border-transparent opacity-50'
+  )
+  const iconList = [
+    {
+      value: 'html',
+      icon: 'i-vscode-icons:file-type-vue'
+    },
+    {
+      value: 'typescript',
+      icon: 'i-vscode-icons:file-type-reactjs'
+    }
+  ]
+  return (
+    <div className="flex items-center text-24px">
+      {iconList.map((item) => (
+        <div
+          key={item.value}
+          className={classNames(
+            props.language === item.value ? activeClass : inactiveClass,
+            'cursor-pointer'
+          )}
+          onClick={() => props.setLanguage(item.value)}
+        >
+          <div className={item.icon} />
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default function EditTemplate(props: {
   visible: boolean
   onClose: () => void
 }) {
   const { templateCode, setTemplateCode } = useConfig()
+  const [language, setLanguage] = useState('html')
   const monacoRef = useMonaco()
   useEffect(() => {
     monacoRef?.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
@@ -30,28 +70,28 @@ export default function EditTemplate(props: {
         transition: 'all ease 0.3s'
       }}
     >
-      <CloseCircleOutlined
-        className="cursor-pointer text-24px position-absolute top-5px right-20px z-1000 color-primary"
-        onClick={props.onClose}
-      />
+      <div className="text-24px position-absolute top-5px right-20px z-1000 flex items-center">
+        <LanguageSelect language={language} setLanguage={setLanguage} />
+        <CloseCircleOutlined
+          className="cursor-pointer text-24px ml-10px color-primary"
+          onClick={props.onClose}
+        />
+      </div>
+
       <Editor
         options={{
-          fontSize: 13,
+          fontSize: 14,
           minimap: {
             enabled: false
           }
         }}
-        defaultLanguage="typescript"
-        theme="vs-dark"
+        language={language}
+        defaultLanguage="html"
         className="flex-1 mb-10px"
         value={templateCode}
+        theme="vs-dark"
         onChange={(val) => {
           setTemplateCode(val || '')
-        }}
-        onValidate={(markers) => {
-          markers.forEach((marker) =>
-            console.log('onValidate:', marker.message)
-          )
         }}
       />
     </div>
